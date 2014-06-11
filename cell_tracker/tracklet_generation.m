d2103e7c14682f08a93f77d1b73428321eed9fd3
Pedro Kostelec <pedrokost@gmail.com>
2014-06-11 10:35:37 +0100

    Tracklets genreations updates/adds new tracks

--- cell_tracker/tracklet_generation.m
+++ cell_tracker/tracklet_generation.m
@@ -4,9 +4,9 @@ folder = fullfile('..', 'cell_detector', 'kidney', 'outKidneyRed');
 
 
 matfiles = dir(fullfile(folder, 'im4*.mat'));
-matfiles = matfiles(1:3);
+matfiles = matfiles(1:4);
 nFrames = length(matfiles);
-nTracklets = 3; % estimate
+nTracklets = 4; % estimate
 
 % create the big matrix of tracklets
 tracklets = zeros(nTracklets, nFrames, 2);
@@ -16,14 +16,14 @@ matfileB = matfiles(1);
 % load(fullfile(folder, matfileB.name));
 % XB = X; dotsB = dots; nCellsB = nCells;
 
-dotsB = [1 1; 2 2; 3 3]; nCellsB = 3;
+dotsB = [1 1; 3 3]; nCellsB = 2;
 
 tracklets(1:nCellsB, 1, :) = dotsB;
 nextID = nCellsB + 1;
 Tprev = [1:nCellsB; 1:nCellsB]';  % previous projection table
 
 Tcurr = []; % current projection table
-nFrames = 3;
+nFrames = 4;
 
 for f=1:nFrames-1
 	%----------------------------------------------------------------Load data
@@ -35,7 +35,9 @@ for f=1:nFrames-1
 	if f == 1
 		dotsB = [2 2; 1 1; 3 3]; nCellsB = 3;
 	elseif f==2
-		dotsB = [3 3; 1 1; 2 2]; nCellsB = 3;
+		dotsB = [3 3; 1 1]; nCellsB = 2;
+	elseif f==3
+		dotsB = [3 3; 2 2; 1 1]; nCellsB = 3;
 	end
 		
 	% load(fullfile(folder, matfileB.name));
@@ -45,10 +47,16 @@ for f=1:nFrames-1
 	%-----------------------------------------------------Find matches A <-> B
 	% [symm right left selected] = match(XA, XB, dotsA, dotsB);
 	if f==1
-		symm = [2;1;3];
+		symm = [2; 3];
+		selected = [0;1;1];
 	elseif f==2
-		symm = [3;2;1];
+		symm = [0;2;1];
+		selected = [1;1];
+	elseif f==3
+		symm = [1;3];
+		selected = [1;0;1];
 	end
+
 		
 	%---------------------------------------------------Update existing tracks
 	Tcurr = zeros(nCellsA, 2);
@@ -56,6 +64,7 @@ for f=1:nFrames-1
 	Icurr = 1:nCellsA;
 
 	for i=Icurr
+		if ~symm(i); continue; end
 		% find match in Tprev(:, 2)
 		matchIndex = find(i == Tprev(:, 2));  % should return only 1 index
 		% take the corresponding index in Tprev(:, 1)
@@ -64,30 +73,18 @@ for f=1:nFrames-1
 		end
 	end
 
-	Tcurr
-	dotsA
-	dotsB
-
 	% Place the data into tracklets
 	for i=1:size(Tcurr, 1)
-		
-		
+		if any(Tcurr(i, :)==0); continue; end
 		fprintf('Take [%d, %d] and update tracklet %d\n', dotsB(Tcurr(i, 2), :), Tcurr(i, 1))
 		tracklets(Tcurr(i, 1), f+1, :) = dotsB(Tcurr(i, 2), :);
 	end
-
-
-	% for cellA=1:nCellsA
-	% 	cellB = perm(cellA);
-	% 	if ~cellB; continue; end
-	% 	tracklets(cellA, f+1, :) = dotsB(cellB, :);
-	% end
 	%-----------------------------------------------------------Add new tracks
-	% newCells = dotsB(~selected, :)
-	% numNewCells = size(newCells, 1) - 1;
-	% tracklets(nextID:(nextID+numNewCells), f+1, :) = newCells;
-	% nextID = nextID+numNewCells+1;
-
+	newCells = dotsB(~selected, :);
+	numNewCells = size(newCells, 1);
+	tracklets(nextID:(nextID+numNewCells-1), f+1, :) = newCells;
+	nextID = nextID+numNewCells;
+	%------------------------------------------Use current data for next frame
 	Tprev = Tcurr;
 end
 tracklets
@@ -95,4 +92,4 @@ tracklets
 
 % display tracklets matrix
 
-trackletViewer(tracklets, struct('animate', false))
+trackletViewer(tracklets, struct('animate', true, 'animationSpeed', 5))
