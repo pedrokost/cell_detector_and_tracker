a3435a519dc674ad92dc31b615dcd07358736d2a
Pedro Kostelec <pedrokost@gmail.com>
2014-06-29 22:00:01 +0100

    Mini refactor and mini fixes (inimportant here)

--- cell_tracker/generate_tracklets_example.m
+++ cell_tracker/generate_tracklets_example.m
@@ -4,6 +4,7 @@ figure(1); clf;
 folderOUT = fullfile('..', 'data', 'series30greenOUT');
 
 tracklets = generateTracklets(folderOUT);
+% tracklets(:, 30:end, 1)
 
 % display tracklets matrix
 trackletViewer(tracklets, struct('animate', false, 'animationSpeed', 5));
