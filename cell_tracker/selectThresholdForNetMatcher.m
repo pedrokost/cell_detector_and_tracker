34c733c4e532e1be790c5256740900cc83b62f4c
Pedro Kostelec <pedrokost@gmail.com>
2014-07-04 17:06:03 +0100

    Trained neural network to match cells

---
+++ cell_tracker/selectThresholdForNetMatcher.m
@@ -0,0 +1,18 @@
+load(fullfile('..', 'data', 'series30greenOUT', 'matcherTrainMatrix.mat'))
+x = X(:, 1:end-1)';
+labels = X(:, end)';
+
+min_recall = 0.98;
+
+scores = matcherClassifier(x);
+
+[Xpr,Ypr,Tpr,AUCpr, OPTROCPT] = perfcurve(labels, scores, 1);
+
+thr = sum((Ypr >= min_recall)) / numel(Xpr)
+% X is FPR (1 - specificity)
+% Y is TPR (recall)
+
+OPTROCPT
+plot(Xpr,Ypr)
+xlabel('1-specificity'); ylabel('Recall')
+title(['Precision-recall curve (AUC: ' num2str(AUCpr) ')'])	
