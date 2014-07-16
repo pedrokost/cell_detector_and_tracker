70280d8af97b4d82020ef36269d730bbfbbe3337
Pedro Kostelec <pedrokost@gmail.com>
2014-07-16 09:57:29 +0100

    Dataset config in 1 config file

---
+++ cell_tracker/loadDatasetInfo.m
@@ -0,0 +1,58 @@
+function dataParams = loadDatasetInfo(dataset)
+%This is used to setup (and load) the parameters of the dataset; use as
+%template for new datasets.
+%
+%INPUT
+%   dataSet = identifier of the data set as set in the switch-case
+% OUTPUT
+%   dataParams = a struct containing
+%         imExt      =  image extension
+%         imPrefix   =  image prefix
+%         imDigits   =  number of digits in image name
+%         dataFolder =  folder that contains the data (if training data, it must contain the both the images and the annotations)
+%         outFolder  = folder to save results and intermediary data
+    
+%Defaults
+imPrefix = 'im';
+imExt = 'pgm';
+imDigits = 3;
+rootFolder = fullfile('..', 'data');
+
+switch dataset
+    case 1 %PhaseContrast
+        %-TRAINING DATA SET-%
+        dataFolder = fullfile(rootFolder, 'phasecontrastIN');
+        outFolder = fullfile(rootFolder, 'phasecontrastOUT');
+    case 2 %LungGreen
+        %-TRAINING DATA SET-%
+        dataFolder = fullfile(rootFolder, 'series30green');
+        outFolder = fullfile(rootFolder, 'series30greenOUT');
+    case 3 %LungRed
+        %-TRAINING DATA SET-%
+        dataFolder = fullfile(rootFolder, 'lungredIN');
+        outFolder = fullfile(rootFolder, 'lungredOUT');
+    case 4 %KidneyGreen
+        %-TRAINING DATA SET-%
+        dataFolder = fullfile(rootFolder, 'kidneygreenIN');
+        outFolder = fullfile(rootFolder, 'kidneygreenOUT');
+    case 5 %KidneyRed
+        %-TRAINING DATA SET-%
+        dataFolder = fullfile(rootFolder, 'kidneyredIN');
+        outFolder = fullfile(rootFolder, 'kidneyredOUT');
+end
+
+if exist(dataFolder,'dir') ~= 7
+    error('Data folder not found')
+end
+
+if exist(outFolder,'dir') ~= 7
+    error('Data OUT folder not found')
+end
+
+dataParams.imExt      =      imExt;
+dataParams.imPrefix   =   imPrefix;
+dataParams.imDigits   =   imDigits;
+dataParams.dataFolder = dataFolder;
+dataParams.outFolder  =  outFolder;
+
+end
