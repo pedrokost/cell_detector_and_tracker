5b868a1f37499b35525e42ca58335fae57f3bbe3
Pedro Kostelec <pedrokost@gmail.com>
2014-07-29 16:04:12 +0100

    Wrapper function to create training matrices and train the models

---
+++ cell_tracker/trainRobustTrackerModel.m
@@ -0,0 +1,27 @@
+function trainRobustTrackerModel(storeID, options)
+	% Generates a training matrix and trains the robust tracklet model for
+	% generating robust tracklets
+	% Input:
+	% 	storeID = a string indicating from which dataset to load the data
+	% 	options = a struct containing parameters such as
+	% 		algorithm = which algorithm to use to train the model
+	% 		outputFileMatrix = where to store the intermediate matrix
+	% 		outputFileModel = where to store the learned model
+	% Output: /
+
+	rng(1234);
+
+	prepareFeatureMatrixForRobustMatcher(options.outputFileMatrix)
+
+	dataFile = options.outputFileMatrix;
+	modelFile = options.outputFileModel;
+
+	switch options.algorithm
+		case 'ANN'
+			trainMatcherRobustClassifierANN(dataFile, modelFile)
+		case 'NB'
+			trainMatcherRobustClassifierNB(dataFile, modelFile)
+		otherwise
+			error('Please specify a robust trackler training algorithm in the configuration file')
+	end
+end
