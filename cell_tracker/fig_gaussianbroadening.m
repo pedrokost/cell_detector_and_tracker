aaa4bbb6144cee66d3faf250bb5dbe25ca54cddd
Pedro Kostelec <pedrokost@gmail.com>
2014-07-31 15:31:00 +0100

    Gaussbroadening graphics for thesis

--- cell_tracker/fig_gaussianbroadening.m
+++ cell_tracker/fig_gaussianbroadening.m
@@ -8,6 +8,7 @@
 % Note: the results of the model evaluation are not normalized, so it is not possible to compare directly model genreated with a different number of extruded tracks.
 
 addpath('lightspeed')
+addpath('export_fig')
 
 clear all 
 prof = false;
@@ -18,45 +19,47 @@ if prof
 
 	profile on
 end
+
+% subplot(1,2,1);
 % clear all;
 
-track5 = [
-	54    324;
-	53    324;
-];
-track6 = [
-	252    293
-];
-track8 = [
-	60    333;
-	61    331;
-	64    337;
-	71    335;
-];
-track9 = [
-	258    301;
-	258    302;
-	258    305;
-	262    307;
-];
+% track5 = [
+% 	54    324;
+% 	53    324;
+% ];
+% track6 = [
+% 	252    293
+% ];
+% track8 = [
+% 	60    333;
+% 	61    331;
+% 	64    337;
+% 	71    335;
+% ];
+% track9 = [
+% 	258    301;
+% 	258    302;
+% 	258    305;
+% 	262    307;
+% ];
 
 % 5 -> 8 (9)
 % 6 -> 9 (8)
-trackA = track5;
-trackB = track8;
-trackC = track9;
-% trackA = [
-% 	% 10 10;
-% 	% 10.1 11;
-% 	% 10.05 12;
-% 	10.2 12.5;
-% 	10.5 14;
-% 	10.8 14.2;
-% 	11 15;
-% 	11.5 16;
-% 	0 0;
-% 	11.2 16.9
-% ];
+% trackA = track5;
+% trackB = track8;
+% trackC = track9;
+trackA = [
+	10 10;
+	10.1 11;
+	10.05 12;
+	10.2 12.5;
+	10.5 14;
+	10.8 14.2;
+	11 15;
+	11.5 16;
+	0 0;
+	11.2 16.9
+]; trackA = fliplr(trackA);
 
 % trackletA2D = [
 %   95   410
@@ -88,10 +91,10 @@ end
 trackA = getTail(trackA, 50);
 model = gaussianBroadeningModel(trackA, 10);  % TODO: 3 fails
 
-minx = 7;
-maxx = 15;
-miny = 290;
-maxy = 340;
+miny = 7;
+maxy = 15;
+minx = 10;
+maxx = 35;
 
 % minx = 245;
 % maxx = 260;
@@ -105,10 +108,10 @@ maxy = 340;
 % maxy = 410;
 
 
-minx = 10;
-maxx = 300;
-miny = 290;
-maxy = 410;
+% minx = 10;
+% maxx = 300;
+% miny = 290;
+% maxy = 410;
 
 
 resolution = 150;
@@ -132,10 +135,10 @@ X = [X1(:) X2(:)];
 % subplot(1,2,2);
 
 if ~prof
+	hold on;
 	contour(X1, X2, reshape(pointsVals,resolution, resolution)); axis equal; axis tight;
 
-	hold on;
-	plot(trackA(:, 1), trackA(:, 2), 'ro-');  axis equal; axis tight; 
+	plot(trackA(:, 1), trackA(:, 2), 'rd-', 'MarkerFaceColor', 'r', 'LineWidth', 2);  axis equal; axis tight; 
 	xlabel('x');
 	ylabel('y');
 
@@ -143,17 +146,17 @@ if ~prof
 end 
 %---------------------------------------------
 
-% trackB = [
-% 	11 18;
-% 	11.1 19;
-% 	11.05 20;
-% 	11.2 20.5;
-% 	11.5 22;
-% 	11.8 22.2;
-% 	12 23;
-% 	12.5 24;
-% 	12 24.7
-% ];
+trackB = [
+	11 18;
+	11.1 19;
+	11.05 20;
+	11.2 20.5;
+	11.5 22;
+	11.8 22.2;
+	12 23;
+	12.5 24;
+	12 24.7
+]; trackB = fliplr(trackB);
 
 % trackletB2D = [
 %  131   395
@@ -175,38 +178,76 @@ trackB = getHead(trackB, 50);
 % end
 val
 
+trackC = [
+	11.7 17;
+	12.1 17.7;
+	12.8 18.9;
+	13.4 18.7;
+	13.9 19.7;
+	14.6 20.8;
+	15.0 21.5;
+	15.4 21.7;
+]; trackC = fliplr(trackC);
+
 if ~prof
 	hold on;
-	plot(trackB(:, 1), trackB(:, 2), 'bo-');  axis equal; axis tight;
+	plot(trackB(:, 1), trackB(:, 2), 'ko-', 'MarkerFaceColor', 'k', 'LineWidth', 2);  axis equal; axis tight;
 end
 
 [val, pointsVals] = evaluateGaussianBroadeningModel(model, trackC);
 val
 
-% if ~prof
-% 	hold on;
-% 	plot(trackC(:, 1), trackC(:, 2), 'go-');  axis equal; axis tight;
+if ~prof
+	hold on;
+	plot(trackC(:, 1), trackC(:, 2), 'go-', 'MarkerFaceColor', 'g', 'LineWidth', 2);  axis equal; axis tight;
 
-% 	% legend('model values', 'intial tracklet', 'estimated tracklet path', 'candidate tracklet A', 'candidate tracklet B')
-% 	title('Gaussian Broadening')
-% end
+	legend('model values', 'intial tracklet', 'estimated tracklet path', 'candidate tracklet A', 'candidate tracklet B', 'Location','southeast')
+	% title('Gaussian Broadening or moving cell')
+end
+
+axis off;
+
+fprintf('Press any key to save plot and show second graphic\n')
+pause
+export_fig ../thesis/images/fig_gaussianbroadening1 -pdf -transparent -painters
+
+cla; clf;
+
+trackA = [0 0];
+
+minx = -5;
+maxx = 5;
+miny = -5;
+maxy = 5;
+
+
+resolution = 150;
+[X1,X2] = meshgrid(linspace(minx, maxx,resolution)',...
+				   linspace(miny, maxy,resolution)');
+
+X = [X1(:) X2(:)];
+
+model = gaussianBroadeningModel(trackA, 10); 
+[val, pointsVals] = evaluateGaussianBroadeningModel(model, X);
+
+if ~prof
+	hold on;
+	contour(X1, X2, reshape(pointsVals,resolution, resolution)); axis equal; axis tight;
+
+	plot(trackA(:, 1), trackA(:, 2), 'rd-', 'MarkerFaceColor', 'r', 'LineWidth', 2);  axis equal; axis tight; 
+	xlabel('x');
+	ylabel('y');
+
+	% legend('model values', 'single cell detection')
+end 
+axis off;
+
+fprintf('Press any key to save plot\n')
+pause
+export_fig ../thesis/images/fig_gaussianbroadening2 -pdf -transparent -painters
 
 
 if prof
 	profile off
 	profile viewer
 end
-
-
-% % valuateGaussianBroadeningModel2013.525 s0.223
-% smvnpdf2010 3.302 s 3.055
-% scholcov2010 0.247 s 0.247
-% sgaussianBroadeningModel 1 0.002 s 0.002
-% sinterp 110.001 s0.001 s
-
-% % evaluateGaussianBroadeningModel2013.090 s0.508
-% smvnormpdf2010 2.582 s0.977
-% smvnormpdfln2010 1.606 s0.768
-% ssolve_tril (MEX-file)2010 0.617 s 0.617
-% scol_sum2010 0.220 s0.220
-% sgaussianBroadeningModel 1 0.002 s 0.002 s
