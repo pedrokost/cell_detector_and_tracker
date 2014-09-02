a8a823819c1278cc2cb0da12ff572df023aaf7c5
Pedro Kostelec <pedrokost@gmail.com>
2014-09-02 17:07:18 +0100

    ekfukf toolbox

--- +tracker/kalmanExtrapolatedMidpoints.m
+++ +tracker/kalmanExtrapolatedMidpoints.m
@@ -29,60 +29,25 @@ function [endTrackA, startTrackB] = kalmanExtrapolatedMidpoints(trackA, trackB, 
 	if doPlot
 		figure(1); clf; hold on; grid on; view([90, 30, 30]);
 		scatter3(InitialLocationA(1), InitialLocationA(2), frameA-numFramesA+2, 'rx');
-		% scatter3(InitialLocationB(1), InitialLocationB(2), frameB + numFramesB, 'bx');
+		scatter3(InitialLocationB(1), InitialLocationB(2), frameB + numFramesB, 'bx');
 	end
 	
 	InitialEstimateError = [1 1];
 	MotionNoise = [1 1];
 	MeasurementNoise = 1;
 
-
-	StateTransitionModel = [  1     1     0     0;
-     0     1     0     0;
-     0     0     1     1;
-     0     0     0     1];
-
-    MeasurementModel = [1     0     0     0;
-     0     0     1     0]
-    ControlModel = [];
-    State = [10; 0; 10; 0];
-    StateCovariance = [     1     0     0     0;
-     0     1     0     0;
-     0     0     1     0;
-     0     0     0     1];
-    ProcessNoise = [   1     0     0     0;
-     0     1     0     0;
-     0     0     1     0;
-     0     0     0     1
-	];
-
-	MeasurementNoise2 = [1 0; 0 1];
-
-	I3 = eye(3);
-	A = [I3 1*I3; 0*I3 I3]
-
-	return
-
-	%   KALMAN_PARAMS - structure with Kalman Filter parameters
-
-	kalmanParams = struct('process_noise_var2', ProcessNoise, 'measurement_noise_var2', MeasurementNoise2, 'estimate_error_var2', InitialEstimateError);
-	%       process_noise_var2 (Q)
-	%       measurement_noise_var2 (R) 
-	%       estimate_error_var2 (P)
-
+	% kalmanFilterA.StateTransitionModel
+	% kalmanFilterA.MeasurementModel
+	% kalmanFilterA.ControlModel 
+	% kalmanFilterA.State
+	% kalmanFilterA.StateCovariance
+	% kalmanFilterA.ProcessNoise
+	% kalmanFilterA.MeasurementNoise
 
 	
 	if extrapolateA > 0
 		kalmanFilterA = configureKalmanFilter('ConstantVelocity',InitialLocationA,InitialEstimateError,MotionNoise, MeasurementNoise);
-		kalmanFilterA.StateTransitionModel
-		kalmanFilterA.MeasurementModel
-		kalmanFilterA.ControlModel 
-		kalmanFilterA.State
-		kalmanFilterA.StateCovariance
-		kalmanFilterA.ProcessNoise
-		kalmanFilterA.MeasurementNoise
 		for i=1:numFramesA
-			[State, kalmanParams] = tracker.KalmanFilter(State, trackA(i, :), kalmanParams, 'predict')
 			[z_pred, x_pred, P_pred] = predict(kalmanFilterA);
 			if ~all(trackA(i, :) == [0 0])
 				if doPlot
@@ -101,25 +66,25 @@ function [endTrackA, startTrackB] = kalmanExtrapolatedMidpoints(trackA, trackB, 
 		endTrackA = InitialLocationA;
 	end
 
-	% if extrapolateB > 0
-	% 	kalmanFilterB = configureKalmanFilter('ConstantVelocity',InitialLocationB,InitialEstimateError,MotionNoise, MeasurementNoise);
+	if extrapolateB > 0
+		kalmanFilterB = configureKalmanFilter('ConstantVelocity',InitialLocationB,InitialEstimateError,MotionNoise, MeasurementNoise);
 
-	% 	for i=numFramesB:-1:1
-	% 		[z_pred, x_pred, P_pred] = predict(kalmanFilterB);
-	% 		if ~all(trackB(i, :) == [0 0])
-	% 			if doPlot
-	% 				scatter3(trackB(i, 1), trackB(i, 2), frameB+i, 'b+');
-	% 			end
-	% 			correct(kalmanFilterB, trackB(i, :));
-	% 		end
-	% 	end
-	% 	for i=(frameB+extrapolateB-1):-1:(frameB)
-	% 		[startTrackB] = predict(kalmanFilterB);
-	% 		if doPlot
-	% 			scatter3(startTrackB(1), startTrackB(2), i-1, 'bo'); hold on;
-	% 		end
-	% 	end
-	% else
-	% 	startTrackB = InitialLocationB;
-	% end
+		for i=numFramesB:-1:1
+			[z_pred, x_pred, P_pred] = predict(kalmanFilterB);
+			if ~all(trackB(i, :) == [0 0])
+				if doPlot
+					scatter3(trackB(i, 1), trackB(i, 2), frameB+i, 'b+');
+				end
+				correct(kalmanFilterB, trackB(i, :));
+			end
+		end
+		for i=(frameB+extrapolateB-1):-1:(frameB)
+			[startTrackB] = predict(kalmanFilterB);
+			if doPlot
+				scatter3(startTrackB(1), startTrackB(2), i-1, 'bo'); hold on;
+			end
+		end
+	else
+		startTrackB = InitialLocationB;
+	end
 end
